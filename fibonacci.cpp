dbf83567b42b0f8cf835c80481e24464b657eb34
Aishwarya Mittal <54315524+aishdolly20@users.noreply.github.com>
2020-05-15 23:08:31 +0530

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin>>n;
+   long long int a[n];
+    a[0]=0;
+    a[1]=1;
+    for(long long int i=2;i<=n;i++)
+    a[i]=(a[i-1])+(a[i-2]);
+    //cout<<a[n]%10;
+    cout<<a[n];
+    return 0;
+}
