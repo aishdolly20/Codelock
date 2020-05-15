3e231985e45c12c9f9c5efb1b99792eb288f9cf3
Aishwarya Mittal <54315524+aishdolly20@users.noreply.github.com>
2020-05-15 23:09:49 +0530

    Add files via upload

---
+++ LCM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+long long int gcd(long long int,long long int);
+int main()
+{
+    long long int a,b;
+    cin>>a>>b;
+    cout<<(a*b/gcd(a,b));
+    return 0;
+}
+long long int gcd(long long int a,long long int b){
+    long long int r;
+    if(b==0)
+    return a;
+    r=a%b;
+    gcd(b,r);
+}  
